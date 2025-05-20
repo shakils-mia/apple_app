e0184b5cf059e08cef1a56242c5552a9f0c870d8
Raihan Akter Liza <liza@greengenesisbd.com>
2025-05-20 16:46:16 +0600

    update model

--- app/Models/ProductWish.php
+++ app/Models/ProductWish.php
@@ -12,5 +12,6 @@ class ProductWish extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class);
     }
 }
