5497edc4d081e6d7f4710f6c94376dae91efae8a
Raihan Akter Liza <liza@greengenesisbd.com>
2025-05-20 16:42:57 +0600

    update Product

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,16 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use SebastianBergmann\CodeCoverage\Report\Html\CustomCssFile;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
+class user extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = ['email', 'otp'];
+    public function profile(): HasOne
+    {
+        return $this->hasOne(CustomerProfile::class);
+    }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
 }
